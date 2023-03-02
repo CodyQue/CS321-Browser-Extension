ab4c0b32cdc141660ebc278b4ac2dfdd8831ec11
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2023-03-02 09:40:13 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 /**
  * 
- * Background.js is used for event handling, such as pressing a button or scrolling. Put all event handling functions here.
+ * Background.js is used for event handling, such as pressing a button or scrolling.
  * This will be essential for the functionality of the browser extension.
+ * Put every action/event handler/callback in this file.
  * 
  */
