bb4da55868288d6b62d183044ac5f2b304e875ae
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2023-03-16 16:15:01 -0400

    Creating Database
    Moved Professor's names and URLs to a database. I will add more attributes later on development.

---
+++ scraping/viewDB.js
@@ -0,0 +1,15 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database('./profURL.db', sqlite3.OPEN_READWRITE, (err)=> {
+    if (err) return console.error(err.message);
+    console.log("Connected to database");
+});
+
+const sql = 'SELECT * FROM Professor_Info';
+
+db.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+        console.log(row);
+    });
+});
