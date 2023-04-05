cd533219d42409c415c7d699ee8f3968dda7e100
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2023-04-05 09:40:39 -0400

    Structuring UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/menu.html"],
+  content: ["./interface/menu.html", "./interface/findProfessor.html", "./interface/randomSchedule.html"],
   theme: {
     extend: {},
   },
